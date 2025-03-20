583a2d40f614f07d3ab649a4529d013e2641793a
zhangshuhan15 <140571036+zhangshuhan15@users.noreply.github.com>
2025-03-20 15:32:29 +0800

    增加解密功能

--- src/main/java/com/psbc/psf/GatewayApplication.java
+++ src/main/java/com/psbc/psf/GatewayApplication.java
@@ -1,11 +1,13 @@
 package com.psbc.psf;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableEncryptableProperties
 public class GatewayApplication {
 
     public static void main(String[] args) {
